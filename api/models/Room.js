5c2ec8eebc61f9e6d7cc1d1411dd0ae2892d9b72
Nguyễn Hữu Thức <thuckechsu02@gmail.com>
2015-10-07 07:11:29 +0700

    .....

--- api/models/Room.js
+++ api/models/Room.js
@@ -1,5 +1,5 @@
 /**
-* User.js
+* Room.js
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
@@ -8,14 +8,6 @@
 module.exports = {
 
   attributes: {
-    name: {
-      type: 'string',
-      required: true
-    },
-    room:{
-      collections: 'room',
-      via: 'users'
-    }
 
   }
 };
